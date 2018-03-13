429d7d2178184f99e2f8f98928448a82620e07f4
yaolinxia <18860976931@163.com>
2018-03-13 16:04:54 +0800

    20180313——leetcode04
    参考网址http://www.cnblogs.com/zuoyuan/p/3759682.html

--- leetcode04.py
+++ leetcode04.py
@@ -1,16 +1,30 @@
 class Solution(object):
-    def findMedianSortedArrays(self, nums1, nums2):
+    def getKth(self, A, B, k):
+        lenA = len(A);
+        lenB = len(B)
+        if lenA > lenB: return self.getKth(B, A, k)
+        if lenA == 0: return B[k - 1]
+        if k == 1: return min(A[0], B[0])
+        pa = min(k / 2, lenA);
+        pb = k - pa
+        if A[pa - 1] <= B[pb - 1]:
+            return self.getKth(A[pa:], B, pb)
+        else:
+            return self.getKth(A, B[pb:], pa)
+
+    def findMedianSortedArrays(self, A, B ):
         """
         :type nums1: List[int]
         :type nums2: List[int]
         :rtype: float
         """
-        len1 = nums1.length
-        len2 = nums2.length
-        median = 0
-        if nums1[len1-1] < nums2[0]:
-            median = nums1[len1-1]+nums2[0]
-        #插入排序
+        lenA = len(A)
+        lenB = len(B)
+        if (lenA + lenB) % 2 == 1:
+            return self.getKth(A, B, (lenA + lenB) / 2 + 1)
         else:
-            for i in nums1:
-                
+            return (self.getKth(A, B, (lenA + lenB) / 2) + self.getKth(A, B, (lenA + lenB) / 2 + 1)) * 0.5
+
+
+
+

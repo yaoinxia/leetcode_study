db55e2919105914de4c851397ac94a2745c91b36
yaoinxia <18860976931@163.com>
2018-03-14 23:57:27 +0800

    Day4 leetcode13

---
+++ 13. Roman to Integer/13. Roman to Integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+
+
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        print(s[1])
+        print(len(s))
+        for i in len(s):
+            if s[i] < 'V'
+
+
+
+
+s = Solution()
+s.romanToInt('IV')

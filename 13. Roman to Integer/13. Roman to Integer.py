fbc19cadfabf6cbdea4b9d2716fcee2ba7b9737c
yaolinxia <18860976931@163.com>
2018-03-15 09:53:58 +0800

    Day5-leetcode13

--- 13. Roman to Integer/13. Roman to Integer.py
+++ 13. Roman to Integer/13. Roman to Integer.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
 # _*_ coding:utf-8 _*_
 
-
-class Solution(object):
+class Solution:
     def romanToInt(self, s):
         """
         :type s: str
         :rtype: int
         """
-        print(s[1])
-        print(len(s))
-        for i in len(s):
-            if s[i] < 'V'
-
-
+        a = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+        sum = 0
+        for i in range(len(s) - 1):
+            if a[s[i]] < a[s[i + 1]]:
+                sum -= a[s[i]]
+            else:
+                sum += a[s[i]]
+        return sum + a[s[len(s) - 1]]
 
 
 s = Solution()
-s.romanToInt('IV')
+print(s.romanToInt('IXL'))

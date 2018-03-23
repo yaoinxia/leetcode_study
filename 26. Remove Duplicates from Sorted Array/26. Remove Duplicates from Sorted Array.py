a14a12f46f099a34ff9bf4620ddd8c99f86f51eb
yaolinxia <18860976931@163.com>
2018-03-23 10:44:23 +0800

    Day11-leetcode26 improve

--- 26. Remove Duplicates from Sorted Array/26. Remove Duplicates from Sorted Array.py
+++ 26. Remove Duplicates from Sorted Array/26. Remove Duplicates from Sorted Array.py
@@ -7,31 +7,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        '''
-        i=0
-        j=1
-        while i < len(nums):
-            while j < len(nums):
-                if nums[i] == nums[j]:
-                    del nums[j]
-                    j=j+1
 
-                else:
-                    j = j + 1
-            i = 0
-            j = 1
-        return nums
-'''
-        result = 0
-        if len(nums) == 0:
-            return 0
-        for i in range(len(nums) - 2):
-            if nums[i + 1] == None:
-                break
-            if nums[i] == nums[i + 1]:
-                del nums[i]
-        return nums
+        i = 0
+        j = 1
 
-
+        while j < len(nums):
+            if nums[i] == nums[j]:
+                del nums[j]
+            else:
+                i = j
+                j = j+1
+        return len(nums)
 s = Solution()
-print(s.removeDuplicates([1,1,2,2,3,3,3,44,44,3,1]))
+print(s.removeDuplicates([1,1,2,2,3,3,3,11,11,13,13,44,44]))

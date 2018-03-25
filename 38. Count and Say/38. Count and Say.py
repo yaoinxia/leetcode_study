54f67d3f0a07ea6025cdf347adefc78b13016a1f
yaoinxia <18860976931@163.com>
2018-03-25 23:57:44 +0800

    Day12

---
+++ 38. Count and Say/38. Count and Say.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+
+class Solution(object):
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        

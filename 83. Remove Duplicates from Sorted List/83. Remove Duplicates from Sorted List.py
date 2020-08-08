ffd13a8b982ff0aa2840b043f97afbc6c6dab3c8
yaolinxia <18860976931@163.com>
2020-08-08 20:53:28 +0800

    leetcode 8 update

--- 83. Remove Duplicates from Sorted List/83. Remove Duplicates from Sorted List.py
+++ 83. Remove Duplicates from Sorted List/83. Remove Duplicates from Sorted List.py
@@ -21,6 +21,28 @@ class Solution(object):
                 p = p.next
         return head
 
+    """
+    给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+    示例 1:
+    
+    输入: 1->1->2
+    输出: 1->2
+    示例 2:
+    
+    输入: 1->1->2->3->3
+    输出: 1->2->3
+    """
+    def delDuplicte(self, head):
+        p = head
+        while p != None and p.next != None:
+            if p.next.val == p.val:
+                p.next =  p.next.next
+            else:
+                p = p.next
+        return head
+
+
+
 if __name__=="__main__":
     l1=ListNode(1)
     l1.next=ListNode(2)
@@ -28,5 +50,6 @@ if __name__=="__main__":
     l1.next.next.next=ListNode(3)
     print(l1)
     s = Solution()
-    s2 = s.deleteDuplicates(l1)
-    print(s2)
+    # s2 = s.deleteDuplicates(l1)
+    s3 = s.delDuplicte(l1)
+    print(s3)

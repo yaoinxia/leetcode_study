bbfb7a2134c93b6397a0b0a952d2d130f16cce9d
yaolinxia <18860976931@163.com>
2020-09-04 07:54:27 +0800

    Create 383. 赎金信.py

---
+++ 383. 赎金信.py
@@ -0,0 +1,48 @@
+"""
+给定一个赎金信 (ransom) 字符串和一个杂志(magazine)字符串，判断第一个字符串 ransom 能不能由第二个字符串
+magazines 里面的字符构成。如果可以构成，返回 true ；否则返回 false。
+
+(题目说明：为了不暴露赎金信字迹，要从杂志上搜索各个需要的字母，组成单词来表达意思。
+杂志字符串中的每个字符只能在赎金信字符串中使用一次。)
+
+注意：
+
+你可以假设两个字符串均只含有小写字母。
+
+canConstruct("a", "b") -> false
+canConstruct("aa", "ab") -> false
+canConstruct("aa", "aab") -> true
+
+"""
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        lr = list(ransomNote)
+        lm = list(magazine)
+        lr.sort()
+        lm.sort()
+        r = "".join(lr)
+        print(r)
+
+        m = "".join(lm)
+        print(m)
+        if m.startswith(r):
+            return True
+        else:
+            return False
+
+    def canConstruct2(self, ransomNote: str, magazine: str) -> bool:
+        dr, dm = {}, {}
+        for c in ransomNote:
+            dr[c] = dr.get(c, 0) + 1
+        for m in magazine:
+            if m in dr:
+                dr[m] -= 1
+        for d in dr:
+            if dr[d] > 0:
+                return False
+            else:
+                continue
+        return True
+
+print(Solution().canConstruct2("bg",
+"efjbdfbdgfjhhaiigfhbaejahgfbbgbjagbddfgdiaigdadhcfcj"))
